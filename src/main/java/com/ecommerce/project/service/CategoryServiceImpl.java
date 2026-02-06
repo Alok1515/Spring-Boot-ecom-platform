910d7159d2f4bdeb2af657f9fe31c794c7ab350e
Alok Kumar <alok@example.com>
2026-02-06 17:25:49 +0530

    CRUD operation

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -17,6 +18,20 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+
+        if(category == null) {
+            return "category not found";
+        }
+        categories.remove(category);
+        return "category with categoryId : " + categoryId + " deleted successfully!!";
     }
 }
