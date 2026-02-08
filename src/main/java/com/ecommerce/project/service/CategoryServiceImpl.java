e4246b80027a22db5fbe5c8b43efcb3c6b91c9d3
Alok Kumar <alok@example.com>
2026-02-08 16:40:58 +0530

    cleaned up the code base

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.model.Category;
@@ -12,19 +13,24 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+
     public CategoryServiceImpl(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty())
+            throw new APIException("No category created till now");
+        return categories;
     }
 
     @Override
     public void createCategory(Category category) {
-
-        // ✅ DO NOT set ID manually
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if(savedCategory != null)
+            throw new APIException("Category with name " + category.getCategoryName() + " already exists!!");
         categoryRepository.save(category);
     }
 
