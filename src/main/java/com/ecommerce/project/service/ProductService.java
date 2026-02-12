65f1c17773360d5adadec7d8ab41474248a283fe
Alok Kumar <alok@example.com>
2026-02-12 18:42:16 +0530

    get the products

---
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+
+public interface ProductService {
+
+    ProductResponse getAllProducts();
+
+    ProductDTO addProduct(Long categoryId, Product product);
+}
+
