3e46e3887dcc91c580ccc1bbe7edb26c2d148d85
Alok Kumar <alok@example.com>
2026-02-13 15:35:06 +0530

    implemented pagination

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 
 public interface ProductService {
 
-    ProductResponse getAllProducts();
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
     ProductDTO addProduct(Long categoryId, ProductDTO product);
 
-    ProductResponse searchByCategory(Long categoryId);
+    ProductResponse searchByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-    ProductResponse searchProductByKeyword(String keyword);
+    ProductResponse searchProductByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
     ProductDTO updateProduct(Long productId, ProductDTO product);
 
